c521c392436ca63937493b74aed48259dcc5c642
kkhashayar <frankdezz@gmail.com>
2024-05-22 17:39:25 +0200

    changed the fen

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,8 +1,7 @@
 ﻿using Engine;
 using Engine.External_Resources;
 
-string fen = "6k1/5p1p/2Q1p1p1/5n1r/N7/1B3P1P/1PP3PK/4q3 b - - 0 1" +
-    "";
+string fen = "6k1/5p1p/2Q1p1p1/5n1r/N7/1B3P1P/1PP3PK/4q3 b - - 0 1";
 if (String.IsNullOrEmpty(fen))
 {
     fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
