c5ca33beabe4d3547f503e4a3fb03dd72e996d78
kkhashayar <frankdezz@gmail.com>
2024-05-27 17:06:42 +0200

    add in evaluatior

--- Engine/Evaluators.cs
+++ Engine/Evaluators.cs
@@ -5,12 +5,14 @@ namespace Engine
     internal static class Evaluators
     {
 
+        public static int numbersOfWhitePieces { get; set; }
+        public static int numbersOfBlackPieces { get; set; }    
         public static decimal GetByMaterial(int[] chessBoard)
         {
 
 
-            int numberOfWhitePieces = 0;
-            int numberOfBlackPieces = 0;
+            numbersOfWhitePieces = 0;
+            numbersOfBlackPieces = 0;
 
             decimal score = 0;
             for (int i = 0; i < 64; i++)
@@ -20,27 +22,27 @@ namespace Engine
                 if (piece == MoveGenerator.whitePawn)
                 {
                     score += 1 + Tables.Pawns.GetWhiteSquareWeight(i);
-                    numberOfWhitePieces++;
+                    numbersOfWhitePieces++;
                 }
                 else if (piece == MoveGenerator.whiteKnight)
                 {
                     score += 3 + Tables.Knights.GetWhiteSquareWeight(i);
-                    numberOfWhitePieces++;
+                    numbersOfWhitePieces++;
                 }
                 else if (piece == MoveGenerator.whiteBishop)
                 {
                     score += 3.2m + Tables.Bishops.GetWhiteSquareWeight(i);
-                    numberOfWhitePieces++;
+                    numbersOfWhitePieces++;
                 }
                 else if (piece == MoveGenerator.whiteRook)
                 {
                     score += 5;
-                    numberOfWhitePieces++;
+                    numbersOfWhitePieces++;
                 }
                 else if (piece == MoveGenerator.whiteQueen)
                 {
                     score += 9;
-                    numberOfWhitePieces++;
+                    numbersOfWhitePieces++;
                 }
                 else if (piece == MoveGenerator.whiteKing)
                 {
@@ -49,36 +51,36 @@ namespace Engine
                 else if (piece == MoveGenerator.blackPawn)
                 {
                     score -= 1 - Tables.Pawns.GetBlackSquareWeight(i);
-                    numberOfBlackPieces++;
+                    numbersOfBlackPieces++;
                 }
                 else if (piece == MoveGenerator.blackKnight)
                 {
                     score -= 3 - Tables.Knights.GetBlackSquareWeight(i);
-                    numberOfBlackPieces++;
+                    numbersOfBlackPieces++;
                 }
                 else if (piece == MoveGenerator.blackBishop)
                 {
                     score -= 3.2m - Tables.Bishops.GetBlackSquareWeight(i);
-                    numberOfBlackPieces++;
+                    numbersOfBlackPieces++;
                 }
                 else if (piece == MoveGenerator.blackRook)
                 {
                     score -= 5;
-                    numberOfBlackPieces++;
+                    numbersOfBlackPieces++;
                 }
                 else if (piece == MoveGenerator.blackQueen)
                 {
                     score -= 9;
-                    numberOfBlackPieces++;
+                    numbersOfBlackPieces++;
                 }
                 else if (piece == MoveGenerator.blackKing)
                 {
                     score -= 0.5m - Tables.Kings.GetBlackSquareWeight(i);
-                    numberOfBlackPieces++;
+                    numbersOfBlackPieces++;
                 }
             }
             decimal pieceCountFactor = 0.1m; // Adjust this value based on testing
-            score += pieceCountFactor * (numberOfWhitePieces - numberOfBlackPieces);
+            score += pieceCountFactor * (numberOfWhitePieces - numbersOfBlackPieces);
             return score;
         }
     }
