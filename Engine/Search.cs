ad8e2e7b8930beb30beb5ce7a52f803d30bc8ed6
kkhashayar <frankdezz@gmail.com>
2024-05-27 14:59:14 +0200

    update

--- Engine/Search.cs
+++ Engine/Search.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Security;
 
 namespace Engine;
 
@@ -7,7 +6,11 @@ public static class Search
 {
     public static List<MoveObject> GetAllPossibleMoves(int[] board, int turn, bool filter)
     {
-        return MoveGenerator.GenerateAllMoves(board, turn, filter).OrderByDescending(m => m.Priority).ToList();
+        var moves = MoveGenerator.GenerateAllMoves(board, turn, filter);
+
+        var orderedmoves = moves.OrderByDescending(m => m.Priority).ToList();
+
+        return orderedmoves;
     }
 
     public static MoveObject GetBestMove(int[] board, int turn, int maxDepth, TimeSpan maxTime)
@@ -21,7 +24,6 @@ public static class Search
         MoveObject bestMove = default;
         List<MoveObject> allPossibleMoves = GetAllPossibleMoves(board, turn, true);
 
-       
         if (!allPossibleMoves.Any())
         {
             if (turn == 0)
@@ -31,6 +33,7 @@ public static class Search
                 Globals.CheckmateWhite = true;
                 return bestMove;
             }
+
             else if (turn == 1)
             {
                 var whiteMoves = GetAllPossibleMoves(board, 0, true);
@@ -39,27 +42,18 @@ public static class Search
                 return bestMove;
             }
         }
-        // Two kings stalemate check
-        bool onlyKings = board.All(p => p == MoveGenerator.whiteKing || p == MoveGenerator.blackKing || p == MoveGenerator.None) && board.Contains(MoveGenerator.whiteKing) && board.Contains(MoveGenerator.blackKing);
-        if (onlyKings)
-        {
-            Globals.Stalemate = true; 
-        }
-
-        if (allPossibleMoves.Count == 1)
-        {
-            bestMove = allPossibleMoves[0];
-            return bestMove;
-        }
 
         for (int currentDepth = 1; currentDepth <= maxDepth; currentDepth++)
         {
-            foreach (var move in allPossibleMoves)
+
+            for (int i = 0; i < allPossibleMoves.Count; i++)
             {
+                var move = allPossibleMoves[i];
+
                 if (stopwatch.Elapsed >= maxTime)
                 {
-                    Globals.PrincipalVariation.Add(bestMove);
-                    break;
+                    Console.WriteLine("Stopping search due to time limit.");
+                    return bestMove;
                 }
 
                 int[] shadowBoard = (int[])board.Clone();
@@ -67,67 +61,59 @@ public static class Search
                 MoveHandler.MakeMove(shadowBoard, move);
 
                 decimal score;
-    
-
                 if (turn == 0)
                 {
                     score = AlphaBetaMin(currentDepth - 1, alpha, beta, shadowBoard, 1);
-
-                    if (score > alpha)
-                    {
-                        alpha = score;
-                        bestMove = move;
-                    }
-
-                    if (score >= beta)
-                    {
-                        Console.WriteLine($"Pruning: Best Move: {MoveToString(bestMove)} Depth: {currentDepth}");
-                        Globals.PrincipalVariation.Add(bestMove);   
-                        return bestMove;
-                    }
                 }
                 else
                 {
                     score = AlphaBetaMax(currentDepth - 1, alpha, beta, shadowBoard, 0);
-
-                    if (score < beta)
-                    {
-                        beta = score;
-                        bestMove = move;
-                    }
-
-                    if (score <= alpha)
-                    {
-                        Console.WriteLine($"Pruning: Best Move: {MoveToString(bestMove)} Depth: {currentDepth}");
-                        Globals.PrincipalVariation.Add(bestMove);
-                        return bestMove; 
-                    }
                 }
-                
-                MoveHandler.RestoreStateFromSnapshot(); 
 
+                MoveHandler.RestoreStateFromSnapshot();
+         
+                if (allPossibleMoves.Count == 1)
+                {
+                    bestMove = move;
+                    return bestMove;
+                }
+                if (turn == 0 && score > alpha)
+                {
+                    alpha = score;
+                    bestMove = move;
+                }
+                else if (turn != 0 && score < beta)
+                {
+                    beta = score;
+                    bestMove = move;
+                }
+
+                // Return immediately if a decisive score is found
+                if (score >= 99 || score <= -99)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Best move over 99 in {currentDepth}: {MoveToString(bestMove)}");
+                    Globals.PrincipalVariation.Add(bestMove);
+                    return bestMove;
+                }
             }
             Console.WriteLine($"Best Move: {MoveToString(bestMove)} Depth: {currentDepth}");
         }
-        Console.WriteLine($"Final Best Move: {MoveToString(bestMove)}");
+        Console.WriteLine($"Best Move: {MoveToString(bestMove)} ");
         Globals.PrincipalVariation.Add(bestMove);
 
         return bestMove;
     }
 
+
     private static decimal AlphaBetaMax(int depth, decimal alpha, decimal beta, int[] board, int turn)
     {
-        var allPossibleMoves = GetAllPossibleMoves(board, turn, true);
+        var whiteMoves = GetAllPossibleMoves(board, 0, true);   
 
-
-        if(depth == 0)
-        {
-            
-            return Evaluators.GetByMaterial(board, 0,0);
-        }
+        if (depth == 0) return Evaluators.GetByMaterial(board, 0, 0);
 
         decimal bestScore = decimal.MinValue;
-        foreach (var move in allPossibleMoves)
+        foreach (var move in whiteMoves)
         {
             int[] shadowBoard = (int[])board.Clone();
             MoveHandler.RegisterStaticStates();
@@ -135,32 +121,28 @@ public static class Search
 
             decimal score = AlphaBetaMin(depth - 1, alpha, beta, shadowBoard, turn ^ 1);
             MoveHandler.RestoreStateFromSnapshot();
+            
 
             if (score > bestScore)
             {
                 bestScore = score;
                 if (score >= beta)
-                    return beta; // Beta cutoff
+                    return beta;
                 if (score > alpha)
-                    alpha = score; // Alpha update
+                    alpha = score;
             }
         }
-        return bestScore;
+
+        return alpha;
     }
 
     private static decimal AlphaBetaMin(int depth, decimal alpha, decimal beta, int[] board, int turn)
     {
-        var allPossibleMoves = GetAllPossibleMoves(board, turn, true);
+        var blackMoves = GetAllPossibleMoves(board, 1, true);   
+        if (depth == 0) return Evaluators.GetByMaterial(board, 0, 0);
 
-        if(depth == 0)
-        {
-            
-            return -Evaluators.GetByMaterial(board, 0,0);   
-        }
-
-
-        decimal bestScore = decimal.MinValue;
-        foreach (var move in allPossibleMoves)
+        decimal bestScore = decimal.MaxValue;
+        foreach (var move in blackMoves)
         {
             int[] shadowBoard = (int[])board.Clone();
             MoveHandler.RegisterStaticStates();
@@ -168,18 +150,20 @@ public static class Search
 
             decimal score = AlphaBetaMax(depth - 1, alpha, beta, shadowBoard, turn ^ 1);
             MoveHandler.RestoreStateFromSnapshot();
-
+         
             if (score < bestScore)
             {
                 bestScore = score;
                 if (score <= alpha)
-                    return alpha; // Alpha cutoff
+                    return alpha;
                 if (score < beta)
-                    beta = score; // Beta update
+                    beta = score;
             }
         }
-        return bestScore;
+
+        return beta;
     }
+
 
     public static string MoveToString(MoveObject move)
     {
@@ -187,8 +171,10 @@ public static class Search
         string castle = move.ShortCastle ? "O-O" : move.LongCastle ? "O-O-O" : "";
 
         if (!string.IsNullOrEmpty(castle))
+        {
             return castle;
+        }
 
-        return $"{Piece.GetPieceName(move.pieceType)}{Globals.GetSquareCoordinate(move.StartSquare)}-{Globals.GetSquareCoordinate(move.EndSquare)}{promotion}";
+        return $"{Piece.GetPieceName(move.pieceType)}{Globals.GetSquareCoordinate(move.StartSquare)}-{Globals.GetSquareCoordinate(move.EndSquare)}{promotion} ";
     }
 }
