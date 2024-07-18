ba866f7cb2074bb4d19668323328c452f69504e0
Khashayar Nariman <khanar@norkart.no>
2024-07-18 08:18:17 +0200

    fen update

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -13,7 +13,7 @@ using Engine.External_Resources;
 // test fen: 8/8/3k4/8/4R3/3K4/8/8 w - - 0 1     KkR
 // test fen:  8/8/3rk3/8/8/5K2/8/8 b - - 0 1     Kkr
 
-string fen = "br1qr1k1/b1pnnp2/p2p2p1/P4PB1/3NP2Q/2P3N1/B5PP/R3R1K1 w - - 1 0";
+string fen = "5R2/3Q1b2/6kp/1prq1ppN/6P1/7P/5P2/6K1 w - - 0 1";
 
 
 Globals globals = Globals.FenReader(fen);
