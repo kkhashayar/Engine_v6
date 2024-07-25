5280da32d4d5cd3efd09bf7c745821dd120b9cbb
kkhashayar <frankdezz@gmail.com>
2024-07-25 07:37:02 +0200

    removed extra comments, copied test fen strings to notes

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -2,18 +2,7 @@
 using Engine.Core;
 using Engine.External_Resources;
 
-// test fen: 6k1/5p1p/2Q1p1p1/5n1r/N7/1B3P1P/1PP3PK/4q3 b - - 0 1            mate in 3
-// test fen: rn4k1/pp1r1pp1/1q1b4/5QN1/5N2/4P3/PP3PPP/3R1RK1 w - - 1 0       mate in 3
-
-// test fen: r1b1rk2/ppq3p1/2nbpp2/3pN1BQ/2PP4/7R/PP3PPP/R5K1 w - - 1 0      mate in 4
-// test fen: br1qr1k1/b1pnnp2/p2p2p1/P4PB1/3NP2Q/2P3N1/B5PP/R3R1K1 w - - 1 0 mate in 4
-// test fen: rn3rk1/pbppq1pp/1p2pb2/4N2Q/3PN3/3B4/PPP2PPP/R3K2R w KQ - 7 11  mate in 7
-
-
-// test fen: 8/8/3k4/8/4R3/3K4/8/8 w - - 0 1     KkR
-// test fen:  8/8/3rk3/8/8/5K2/8/8 b - - 0 1     Kkr
-
-string fen = "br1qr1k1/b1pnnp2/p2p2p1/P4PB1/3NP2Q/2P3N1/B5PP/R3R1K1 w - - 1 0";
+string fen = "";
 
 
 Globals globals = Globals.FenReader(fen);
@@ -23,16 +12,10 @@ Globals globals = Globals.FenReader(fen);
 // RunPerft(fen, globals, perftDepth);
 //////////////////   PERFT And stockfish verification
 
-
-
 int searchDepth = Globals.MaxDepth;
 Globals.GetGamePhase();
 
 TimeSpan maxTime = TimeSpan.FromSeconds(Globals.ThinkingTime);
-
-//Console.WriteLine($"Max time: {Globals.ThinkingTime}  Max Depth: {searchDepth}");
-//Console.WriteLine("Press a key to continue");
-//Console.ReadKey();  
 
 Run();
 
@@ -54,7 +37,7 @@ void Run()
         move = Search.GetBestMove(globals.ChessBoard, Globals.Turn, searchDepth, maxTime);
         
         MoveHandler.MakeMove(globals.ChessBoard, move);
-        Console.Beep(2000, 100);
+
         Globals.moveHistory.Add(move);  
 
         Globals.Turn ^= 1;
@@ -65,8 +48,6 @@ void Run()
         else if (Globals.InitialTurn == 1) printBoardBlackDown(globals.ChessBoard);
        
         Console.WriteLine();
-       
-
       
         if (Globals.CheckmateWhite || Globals.CheckmateBlack || Globals.Stalemate)
         {
@@ -86,9 +67,6 @@ void Run()
     {
         Console.Write(Globals.MoveToString(move));
     }
-    
-    Console.Beep(1500, 500);
-    Console.Beep(1500, 500);
     Console.ReadKey();
 }
 
@@ -123,7 +101,6 @@ void printBoardBlackDown(int[] board)
 {
     Console.OutputEncoding = System.Text.Encoding.Unicode;
     string[] fileNames = { "H", "G", "F", "E", "D", "C", "B", "A" };
-
 
     for (int rank = 7; rank >= 0; rank--)
     {
