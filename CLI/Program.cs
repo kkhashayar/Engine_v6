ee4257bfdfc06665c2e50101e8d6e310af256bc3
kkhashayar <frankdezz@gmail.com>
2024-08-18 12:50:13 +0200

    a

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -14,7 +14,7 @@ using Engine.External_Resources;
 // test fen: 8/8/3k4/8/4R3/3K4/8/8 w - - 0 1     KkR
 // test fen:  8/8/3rk3/8/8/5K2/8/8 b - - 0 1     Kkr
 
-string fen = "rn4k1/pp1r1pp1/1q1b4/5QN1/5N2/4P3/PP3PPP/3R1RK1 w - - 1 0";
+string fen = "r1b1rk2/ppq3p1/2nbpp2/3pN1BQ/2PP4/7R/PP3PPP/R5K1 w - - 1 0";
 
 
 Globals globals = Globals.FenReader(fen);
